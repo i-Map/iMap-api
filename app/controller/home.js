9d358ba265c247bb703dbbcc22f2ac13f19fe15d
Trevor <xizhouh@gmail.com>
2018-01-17 20:17:15 -0600

    refactor: Test auto-build

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,7 +4,7 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, iMap';
+    this.ctx.body = 'hi, iMap.';
   }
 }
 
