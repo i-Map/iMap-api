3ec73d081b1e6c6e5263cc8432822bc72ad20514
Trevor <xizhouh@gmail.com>
2018-01-17 21:29:17 -0600

    fix: Fix ENV variable undefined

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,14 +4,7 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    const ctx = this.ctx;
-    const id = ctx.query.id;
-    const AV = this.ctx.AV;
-    const user = AV.Object.createWithoutData('_User', id);
-    const result = await user.fetch();
-    ctx.body = {
-      data: result,
-    };
+    ctx.body = 'hi iMap';
     ctx.status = 200;
   }
 }
--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const { app } = require('egg-mock/bootstrap');
+const app = require('egg-mock/bootstrap');
 
 describe('test/app/controller/home.test.js', () => {
   it('should GET /', () => {
     return app.httpRequest()
-      .get('/?id=59dafe35fe88c2003c957aac')
+      .get('/')
+      .expect('hi, iMap')
       .expect(200);
   });
 });
