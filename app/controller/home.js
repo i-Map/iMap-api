d1bc8fb20937dfc0434aca70f6764c7805e18233
Trevor <xizhouh@gmail.com>
2018-01-17 20:44:32 -0600

    fix: app.config.env -> process.env

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,7 +4,15 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, iMap.';
+    const ctx = this.ctx;
+    const id = ctx.query.id;
+    const AV = this.ctx.AV;
+    const user = AV.Object.createWithoutData('_User', id);
+    const result = await user.fetch();
+    ctx.body = {
+      data: result,
+    };
+    ctx.status = 200;
   }
 }
 
--- config/config.prod.js
+++ config/config.prod.js
@@ -2,21 +2,20 @@
 
 /**
  * 生产环境配置项
- * @param {Egg.Application} app - egg应用
  * @param  {Object} appInfo - 应用信息
  * @return {Object} config - 配置项
  */
-module.exports = (app, appInfo) => {
+module.exports = appInfo => {
   const config = exports = {};
 
   // app key
-  config.keys = appInfo.name + app.config.env.IMAP_API_KEY;
+  config.keys = appInfo.name + process.env.IMAP_API_KEY;
 
   // leancloud key
   exports.leancloud = {
-    appId: app.config.env.IMAP_API_LEANCLOUD_APPID,
-    appKey: app.config.env.IMAP_API_LEANCLOUD_APPKEY,
-    masterKey: app.config.env.IMAP_API_LEANCLOUD_MASTERKEY,
+    appId: process.env.IMAP_API_LEANCLOUD_APPID,
+    appKey: process.env.IMAP_API_LEANCLOUD_APPKEY,
+    masterKey: process.env.IMAP_API_LEANCLOUD_MASTERKEY,
   };
 
   return config;
