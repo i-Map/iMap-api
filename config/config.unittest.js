3477acd1f67ae37e68edcc6d4314eec881d8f14b
Trevor <xizhouh@gmail.com>
2018-01-19 22:32:51 -0600

    fix: Fix cant't find Travis-CI ENV variable

---
+++ config/config.unittest.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * 测试环境配置项
+ * @param  {Object} appInfo - 应用信息
+ * @return {Object} config - 配置项
+ */
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // app 秘钥
+  config.keys = appInfo.name + process.env.IMAP_API_KEY || System.getenv(IMAP_API_KEY);
+
+  // leancloud 秘钥
+  exports.leancloud = {
+    appId: process.env.IMAP_API_LEANCLOUD_APPID || System.getenv(IMAP_API_LEANCLOUD_APPID),
+    appKey: process.env.IMAP_API_LEANCLOUD_APPKEY || System.getenv(IMAP_API_LEANCLOUD_APPKEY),
+    masterKey: process.env.IMAP_API_LEANCLOUD_MASTERKEY || System.getenv(IMAP_API_LEANCLOUD_MASTERKEY),
+  };
+
+  // jwt 秘钥
+  config.jwt = {
+    secret: process.env.IMAP_API_SECRET || System.getenv(IMAP_API_SECRET),
+  };
+
+  // GitHub 应用信息
+  config.githubConfig = {
+    IMAP_GITHUB_CLIENT_ID: process.env.IMAP_GITHUB_CLIENT_ID || System.getenv(IMAP_GITHUB_CLIENT_ID),
+    IMAP_GITHUB_CLIENT_SECRET: process.env.IMAP_GITHUB_CLIENT_SECRET || System.getenv(IMAP_GITHUB_CLIENT_SECRET),
+    IMAP_GITHUB_ACCESS_TOKEN_URL: process.env.IMAP_GITHUB_ACCESS_TOKEN_URL || System.getenv(IMAP_GITHUB_ACCESS_TOKEN_URL),
+    IMAP_GITHUB_REDIRECT_URL: process.env.IMAP_GITHUB_REDIRECT_URL || System.getenv(IMAP_GITHUB_REDIRECT_URL),
+    IMAP_GITHUB_USER: process.env.IMAP_GITHUB_USER || System.getenv(IMAP_GITHUB_USER),
+  };
+
+  return config;
+};
